a3ba6a9cfe9ff318ccf9c01ab17d9cf432d8fa24
Robert Buhnerkempe <rbuhner@gmail.com>
2020-11-21 15:32:20 -0500

    Prev changes

--- ntestbot.py
+++ ntestbot.py
@@ -40,3 +40,33 @@ class NTestBot(sc2.BotAI):
         aieye=np.zeros((self.game_info.map_size[1],self.game_info.map_size[0],3), np.uint8)
 
     async def on_step(self, iteration):
+        return None
+
+class nNet():
+    def __init__(self, x, y):
+        self.input = x
+        self.weights1 = np.random.rand(self.input.shape[1], 4)
+        self.weights2 = np.random.rand(4, 1)
+        self.y = y
+        self.output = np.zeroes(y.shape)
+
+    def sig(self, x):
+        return 1/(1+np.exp(-x))
+    def d_sig(self, x):
+        return x*(1-x)
+
+    def feedforward(self):
+        self.layer1 = sigmoid(np.dot(self.input, self.weights1))
+        self.output = sigmoid(np.dot(self.layer1, self.weights2))
+
+    def backprop(self):
+        #Application of the chain rule to find derivative of the loss function
+        #   with respect to weights2 and weights1
+        d_weights2 = np.dot(self.layer1.T, (2*(self.y-self.output)* \
+        sigmoid_derivative(self.output)))
+        d_weights1 = np.dot(self.input.T, (np.dot(2*(self.y-self.output)* \
+        sigmoid_derivative(self.output), self.weights2.T)*sigmoid_derivative(self.layer1)))
+
+        #Update the weights with the derivative(slope) of the loss function
+        self.weights1 += d_weights1
+        self.weights2 += d_weights2
